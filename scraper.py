64a4241d8f50bcdec105710b28cf95a597536d22
estebanlacerda1-jpg <estebanlacerda1@gmail.com>
2026-04-13 09:13:31 -0300

    scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
+base_url = "https://juegosdigitalesuruguay.com/categorias/juegos-digitales-ps4"
+
+juegos = []
+page = 1
+
+while True:
+    url = f"{base_url}?page={page}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        break
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    nombres = soup.find_all("h4")
+    precios = soup.find_all("span", class_="price")
+
+    if not nombres:
+        break
+
+    for nombre, precio in zip(nombres, precios):
+        juegos.append({
+            "Nombre": nombre.text.strip(),
+            "Precio": precio.text.strip()
+        })
+
+    page += 1
+
+df = pd.DataFrame(juegos)
+df.to_excel("juegos_ps4.xlsx", index=False)
